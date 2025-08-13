507415f8c2207e5b29ef57111d0855f8482028f8
TuanDat <158238597+nguthe@users.noreply.github.com>
2025-08-13 14:23:43 +0700

    Add files via upload

---
+++ views/ContactView.php
@@ -0,0 +1,13 @@
+<?php 
+  //load LayoutTrangTrong.php
+  $this->layoutPath = "LayoutTrangTrong.php";
+ ?>
+<div class="template-customer">
+  <h1>Liên hệ</h1>
+  <iframe 
+    src="<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m8!1m3!1d465.6073136368241!2d105.7958935!3d20.9983081!3m2!1i1024!2i768!4f13.1!4m6!3e0!4m0!4m3!3m2!1d20.998337199999998!2d105.7958268!5e0!3m2!1svi!2s!4v1755064416194!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>" 
+    width="100%" 
+    height="450" 
+    style="border:0;" 
+    allowfullscreen="" loading="lazy"></iframe>   
+</div>
